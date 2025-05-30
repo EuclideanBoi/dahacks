e46fb7c6c23a419876ad9761838401cc90c19e81
EuclideanBoi <terencetylee@gmail.com>
2025-05-30 13:43:02 -0700

    documented test.py

--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import app
 from app import Account
 
-#bruh = Account(accountName="test", rootPw="hehe")
+#bruh = Account(accountName="test", rootPw="hehe") # REGISTER account OBJECT
 
-bruh2 = Account("test")
-bruh2.getExisting()
-# bruh2.registerUser("newuser", "newpwd", "hehe")
-bruh2.useUser("newuser", "newpwd")
-bruh2.newTransaction(42069, "initial balance again and again")
+bruh2 = Account("test") # create account OBJECT (just name)
+bruh2.getExisting() # get account info from database and save it locally
+# bruh2.registerUser("newuser", "newpwd", "hehe") # register new user using root info
+bruh2.useUser("newuser", "newpwd") # use user for transactions
+bruh2.newTransaction(42069, "initial balance again and again") # transaction
 
-balanceSheet = bruh2.getTransactions()
+balanceSheet = bruh2.getTransactions() # returns array of transaction info
 
 for x in balanceSheet:
     print(x)
